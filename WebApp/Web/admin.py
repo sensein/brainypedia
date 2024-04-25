b26d189031e3ff88dcf9c8c4f321e6179fb58177
Tek Raj Chhetri <tekrajchhetri@gmail.com>
2024-04-25 13:24:51 -0400

    models registered to the admin interface + new urls registered

---
+++ WebApp/Web/admin.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+from .models import KnowledgeBaseViewerModel, QueryEndpoint
+# Register your models here.
+
+class KnowledgeBaseAdmin(admin.ModelAdmin):
+    list_display = ('left_side_menu_title', 'created_at', 'updated_at', 'status_active', 'left_side_menu_title_slug')
+    list_filter = ('left_side_menu_title',  'status_active')
+
+class QueryEndpointAdmin(admin.ModelAdmin):
+    list_display = ('endpoint_title', 'query_endpoint_type', 'endpoint_service_type', 'created_at', 'status_active')
+    list_filter = ('endpoint_title', 'endpoint_service_type', 'query_endpoint_type', 'status_active')
+
+admin.site.register(QueryEndpoint, QueryEndpointAdmin)
+admin.site.register(KnowledgeBaseViewerModel, KnowledgeBaseAdmin)
