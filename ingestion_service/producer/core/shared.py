671e640765cbd0e258f76ec56a4120057974a322
Tek Raj Chhetri <tekrajchhetri@gmail.com>
2024-04-30 22:11:12 -0400

    test data filter added

--- ingestion_service/producer/core/shared.py
+++ ingestion_service/producer/core/shared.py
@@ -20,20 +20,14 @@
 import json
 
 def has_context(json_obj):
+    """Simple JSON-LD check for presence of the context"""
     return '@context' in json_obj
 
 
 def is_valid_jsonld(jsonld_str):
     try:
         jsonld_obj = json.loads(jsonld_str)
-        return has_context(jsonld_obj)
+        return has_context(jsonld_obj["kg_data"])
     except ValueError:
         return False
 
-
-if __name__=="__main__":
-    jsonld_string = '{"@contexst": "https://schema.org", "@type": "Person", "name": "John Doe"}'
-    if is_valid_jsonld(jsonld_string):
-        print("Valid JSON-LD")
-    else:
-        print("Invalid JSON-LD")
