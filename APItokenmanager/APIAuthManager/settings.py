25516bcb4736eff20abcd5f8613bb2f53b55044e
Tek Raj Chhetri <tekrajchhetri@gmail.com>
2024-05-06 16:20:19 -0400

    requirements.txt and settings updated

--- APItokenmanager/APIAuthManager/settings.py
+++ APItokenmanager/APIAuthManager/settings.py
@@ -12,13 +12,16 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 import os
-
+from dotenv import load_dotenv
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
+env_file = os.path.join(BASE_DIR, ".env")
 
+# Load environment variables from the .env.development file
+load_dotenv(dotenv_path=env_file)
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv("BRAINYPEDIA_APITOKEN_MANAGER_SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
