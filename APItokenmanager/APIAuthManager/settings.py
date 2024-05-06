51b0b71cee41e74c78876b17a7529ad94aa1461b
Tek Raj Chhetri <tekrajchhetri@gmail.com>
2024-05-06 13:07:51 -0400

    secret key load from env

--- APItokenmanager/APIAuthManager/settings.py
+++ APItokenmanager/APIAuthManager/settings.py
@@ -20,8 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-)@-kr16hschn*ezk-=_tfg(pomnf2e*qk&p#avw+^=y-t0ibsu"
-
+SECRET_KEY = os.getenv("BRAINYPEDIA_APITOKEN_MANAGER_SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
